8d4d0f269da06bf88251d52e65b8f2241ce5cb50
qunetol <qunetol57@outlook.com>
2023-01-08 21:34:47 +0300

    Create test, question, answer models

---
+++ database/migrations/2023_01_04_203431_create_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->text('text');
+            $table->integer('level');
+            $table->integer('time');
+            $table->integer('type');
+            $table->foreignId('test_id')->constrained();
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
