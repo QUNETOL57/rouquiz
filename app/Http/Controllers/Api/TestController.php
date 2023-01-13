8d7b7a16b3d776e5674a016237271908fe9d81e3
qunetol <qunetol57@outlook.com>
2023-01-13 22:46:26 +0300

    swap controller to api

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -3,15 +3,68 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Services\TestService;
+use App\Http\Requests\StoreTestRequest;
+use App\Http\Requests\UpdateTestRequest;
+use App\Http\Resources\TestResource;
+use App\Models\Test;
 
 class TestController extends Controller
 {
-    private TestService $testService;
-
-    public function __construct(TestService $testService)
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        $this->testService = $testService;
+        return TestResource::collection(Test::all());
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \App\Http\Requests\StoreTestRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTestRequest $request)
+    {
+        $test = Test::create($request->validated());
+        return $test;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Test $test
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Test $test)
+    {
+        return new TestResource($test);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \App\Http\Requests\UpdateTestRequest $request
+     * @param \App\Models\Test $test
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTestRequest $request, Test $test)
+    {
+        $test->update($request->all());
+        return $test;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Test $test
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Test $test)
+    {
+        $test->delete();
+        return response(null, 204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\TestController;
-use App\Http\Resources\TestResource;
-use App\Models\Test;
+use App\Http\Controllers\Api\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,10 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-//Route::get('/test/{id}', function ($id) {
-//    return new TestResource(Test::findOrFail($id));
-//});
 
 Route::apiResource('/test', TestController::class);
